358ad1ffe3f9eabb73f46b3822febfbb6f21baa9
Edison Wang <edison@molabs.com>
2016-01-11 11:30:45 -0500

    Allow sticky events.

--- eventservice_lib/src/main/java/com/edisonwang/eventservice/lib/PennStation.java
+++ eventservice_lib/src/main/java/com/edisonwang/eventservice/lib/PennStation.java
@@ -41,20 +41,28 @@ public class PennStation {
         return sManager;
     }
 
+    public static void postStickyEvent(Object object) {
+        getInstance().postStickyEvent(object);
+    }
+
     public static void postEvent(Object object) {
-        getInstance().getEventBus().post(object);
+        getInstance().postEvent(object);
     }
 
     public static void registerListener(Object object) {
-        getInstance().getEventBus().register(object);
+        getInstance().registerListener(object);
     }
 
     public static void unRegisterListener(Object object) {
-        getInstance().getEventBus().unregister(object);
+        getInstance().unRegisterListener(object);
     }
 
     public static String requestAction(ActionRequest request) {
         return getInstance().requestAction(request);
+    }
+
+    public <T> T getStickyEvent(Class<T> eventType) {
+        return getInstance().getStickyEvent(eventType);
     }
 
     public static class EventManager {
@@ -80,13 +88,21 @@ public class PennStation {
 
                             if (result != null) {
                                 result.setResponseInfo(new ResponseInfo(b));
-                                postEvent(result);
+                                if (result.postSticky()) {
+                                    postStickyEvent(result);
+                                } else {
+                                    postEvent(result);
+                                }
                             }
                         }
                     };
             mServiceConnection = new EventServiceImpl.EventServiceConnection(context, mServiceResponseHandler);
             context.bindService(new Intent(context, eventServiceClass), mServiceConnection,
                     Context.BIND_AUTO_CREATE);
+        }
+
+        public void postStickyEvent(Object object) {
+            mBus.postSticky(object);
         }
 
         public void postEvent(Object object) {
@@ -103,6 +119,10 @@ public class PennStation {
 
         public EventBus getEventBus() {
             return mBus;
+        }
+
+        public <T> T getStickyEvent(Class<T> eventType) {
+            return mBus.getStickyEvent(eventType);
         }
 
         public String requestAction(ActionRequest request) {
